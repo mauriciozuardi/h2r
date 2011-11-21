f9f6445b726bd9b2fa5a7c8d37c943e648dfbec0
Mauricio Zuardi <mauricio.zuardi@gmail.com>
2011-11-21 19:09:31 -0200

    Divisões da timeline ok!

--- js/script.js
+++ js/script.js
@@ -1,24 +1,70 @@
+//globals
+nLinhas = 0;
+
 $(init);
 
 function init(){
 	//ajusta a altura do body no onload
-	resizedBg();
-	drawEvents();
+	drawTimeline();
+	resizeBg();
 	//ajusta a altura do body no resize
 	$(window).resize(function (event){
-		resizedBg();
-		resizedEvents();
+		resizeBg();
+		resizeEvents();
+		resizeTimeline()
 	});
 }
 
-function resizedBg(){
-	$('.bgPhoto').css('height', $(window).height());
+function resizeBg(){
+	$('#bgPhoto').css('height', $(window).height());
+	$('#photoCover').css('height', $(window).height());
+}
+
+function drawTimeline(){
+	//trata os nomes e atualiza a quantidade de linhas
+	tlLabels_str = 'mês passado,último finde,ontem,hoje,amanhã,fim de semana,mês que vem';
+	tlLabels_str = tlLabels_str.replace(/ /g, '&nbsp;'); // afeta todas as instancias do caractere entre //g
+	timelineLabels = tlLabels_str.split(',');
+	nLinhas = timelineLabels.length;
+	
+	//cria os elementos
+	for(var i in timelineLabels){
+		var html = "<div class='line l" + i + "'><spam><spam class='bullet'>•</spam>" + timelineLabels[i] + "</spam></div>";
+		$(html).appendTo('#timelineGrid');
+	}
+	
+	//ajusta a altura das linhas
+	$('.line').css('top', $('#header').height());
+	$('.line').css('height', $(window).height() - $('#header').height());
+	
+	//posiciona as linhas no eixo x
+	ajustaLinhas();
+}
+
+function resizeTimeline(){
+	//ajusta altura da linha
+	$('.line').css('top', $('#header').height());
+	$('.line').css('height', $(window).height() - $('#header').height());
+	
+	//ajusta posição x das linhas
+	ajustaLinhas();
+}
+
+function ajustaLinhas(){
+	safeMargin = 30;
+	for(var i=0; i<nLinhas; i++){
+		var str = '.line.l' + i;
+		var element = $(str);
+		var step = (($(window).width() - (2*safeMargin))/nLinhas);
+		var position = Math.floor(safeMargin + i * step + (step/2));
+		element.css('left', position);		
+	}
 }
 
 function drawEvents(){
-	
+
 }
 
-function resizedEvents(){
+function resizeEvents(){
 	
 }
