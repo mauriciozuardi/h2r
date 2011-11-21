6c4e5ff927cfbf647dc18d743b25ca71ee2cfda5
Mauricio Zuardi <mauricio.zuardi@gmail.com>
2011-11-21 20:55:32 -0200

    Ajustes com a Helena

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function resizeBg(){
 
 function drawTimeline(){
 	//trata os nomes e atualiza a quantidade de linhas
-	tlLabels_str = 'mês passado,último finde,ontem,hoje,amanhã,fim de semana,mês que vem';
+	tlLabels_str = 'mês passado,último finde,ontem,hoje,amanhã,próximo finde,mês que vem';
 	tlLabels_str = tlLabels_str.replace(/ /g, '&nbsp;'); // afeta todas as instancias do caractere entre //g
 	timelineLabels = tlLabels_str.split(',');
 	nLinhas = timelineLabels.length;
@@ -34,8 +34,9 @@ function drawTimeline(){
 	}
 	
 	//ajusta a altura das linhas
-	$('.line').css('top', $('#header').height());
-	$('.line').css('height', $(window).height() - $('#header').height());
+	// $('.line').css('top', $('#header').height());
+	// $('.line').css('height', $(window).height() - $('#header').height());
+	$('.line').css('height', $(window).height());
 	
 	//posiciona as linhas no eixo x
 	ajustaLinhas();
@@ -43,8 +44,9 @@ function drawTimeline(){
 
 function resizeTimeline(){
 	//ajusta altura da linha
-	$('.line').css('top', $('#header').height());
-	$('.line').css('height', $(window).height() - $('#header').height());
+	// $('.line').css('top', $('#header').height());
+	// $('.line').css('height', $(window).height() - $('#header').height());
+	$('.line').css('height', $(window).height());
 	
 	//ajusta posição x das linhas
 	ajustaLinhas();
@@ -55,7 +57,8 @@ function ajustaLinhas(){
 	for(var i=0; i<nLinhas; i++){
 		var str = '.line.l' + i;
 		var element = $(str);
-		var step = (($(window).width() - (2*safeMargin))/nLinhas);
+		var w = Math.max(960, $(window).width());
+		var step = ((w - (2*safeMargin))/nLinhas);
 		var position = Math.floor(safeMargin + i * step + (step/2));
 		element.css('left', position);		
 	}
