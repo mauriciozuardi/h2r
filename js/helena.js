e7862ba49175f2d92750334170551609383995b4
Mauricio Zuardi <mauricio.zuardi@gmail.com>
2011-11-23 12:40:03 -0200

    Morra!

--- js/helena.js
+++
@@ -1,5 +0,0 @@
-timeMarksStr = 'mês passado|último finde|ontem|hoje|amanhã|próximo finde|mês que vem|fim do mundo=December 21, 2012 12:06:00';
-// aDay = 'July 14, 1977 22:41:03';
-aDay = 'hoje';
-showDateDetails = true;
-imgName = 'logo-agenda-de-fotografia.png';
